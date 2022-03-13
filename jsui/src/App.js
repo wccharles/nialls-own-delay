af382fdc244d444dc9113c29c6687632670237a1
WCCharles <wccharles@linux.com>
2022-03-13 16:18:10 +0000

    removed adsr from UI

--- jsui/src/App.js
+++ jsui/src/App.js
@@ -107,42 +107,6 @@ class App extends Component {
           </ParameterSlider>
 
           <ParameterSlider
-            paramId="Attack"
-            onDraw={Slider.drawRotary(sliderTrackColor, sliderFillColor)}
-            mapDragGestureToValue={Slider.rotaryGestureMap}
-            {...styles.knob}
-          >
-            <Label paramId="Attack" {...styles.label} />
-          </ParameterSlider>
-
-          <ParameterSlider
-            paramId="Decay"
-            onDraw={Slider.drawRotary(sliderTrackColor, sliderFillColor)}
-            mapDragGestureToValue={Slider.rotaryGestureMap}
-            {...styles.knob}
-          >
-            <Label paramId="Decay" {...styles.label} />
-          </ParameterSlider>
-
-          <ParameterSlider
-            paramId="Sustain"
-            onDraw={Slider.drawRotary(sliderTrackColor, sliderFillColor)}
-            mapDragGestureToValue={Slider.rotaryGestureMap}
-            {...styles.knob}
-          >
-            <Label paramId="Sustain" {...styles.label} />
-          </ParameterSlider>
-
-          <ParameterSlider
-            paramId="Release"
-            onDraw={Slider.drawRotary(sliderTrackColor, sliderFillColor)}
-            mapDragGestureToValue={Slider.rotaryGestureMap}
-            {...styles.knob}
-          >
-            <Label paramId="Release" {...styles.label} />
-          </ParameterSlider>
-
-          <ParameterSlider
             paramId="FreqShift"
             onDraw={Slider.drawLinearHorizontal(sliderTrackColor, sliderFillColor)}
             mapDragGestureToValue={Slider.linearHorizontalGestureMap}
