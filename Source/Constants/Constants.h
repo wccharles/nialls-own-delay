0003ede709712cb2b8a6d5b39166b57bd1adeeb6
WCCharles <wccharles@linux.com>
2022-03-28 22:18:32 +0100

    rm lforate

--- Source/Constants/Constants.h
+++ Source/Constants/Constants.h
@@ -50,7 +50,6 @@ namespace ModDelay
         static constexpr auto fdnSize = 16;
         static constexpr auto maximumPreDelayInSeconds = 0.25f;
         static constexpr auto minimumReverbDecay = 0.25f;
-        static float          lfoRates[] = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f };
         static float          exponentialDelayTimes[] = { 0.01f, 0.0128083f, 0.0164052f, 0.0210122f, 0.026913f, 0.034471f, 0.0441513f, 0.0565503f, 0.0724311f, 0.0927718f, 0.118825f, 0.152194f, 0.194934f, 0.249677f, 0.319793f, 0.4096f };
     } // namespace FDNConstants
 }
